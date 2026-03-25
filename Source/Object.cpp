8024a2c21375670020c3c73ee162ced2d29790da
REIREIREIMU <240255jc-21.jp>
2026-03-25 09:28:03 +0900

    プレイシーンのステージ一面地面で埋め尽くしたよ

--- Source/Object.cpp
+++ Source/Object.cpp
@@ -1,15 +1,15 @@
 #include "Object.h"
 
-Object::Object() :Block_m_handle(-1),
-pos_x(0.0f), pos_y(0.0f), pos_z(0.0f)
+Object::Object() :
+	Block_m_handle(-1), pos_x(0.0f), pos_y(0.0f), pos_z(0.0f),
+	Ground_m_handle(-1), Half_Size(50), Ground_Size(1.0f)
 {
 	// 3Dモデルの読み込み
 	Block_m_handle  = MV1LoadModel("Assets/Block.mv1");
-	//Ground_m_handle = MV1LoadModel("Assets/Ground.mv1");
+	Ground_m_handle = MV1LoadModel("Assets/Ground.mv1");
 
 	// モデルの位置を設定する
-	MV1SetPosition(Block_m_handle,  VGet(pos_x, pos_y, pos_z));	      // 位置座標
-	//MV1SetPosition(Ground_m_handle, VGet(pos_x , pos_y, pos_z));	  // 位置座標
+	MV1SetPosition(Block_m_handle,  VGet(pos_x, pos_y, pos_z));	 // 位置座標
 }
 
 Object::~Object()
@@ -25,7 +25,20 @@ void Object::Update()
 
 void Object::Draw()
 {
-	// モデルの表示
+	// 地面を指定範囲内一面に埋めて表示
+    for (int x = -Half_Size; x <= Half_Size; x++)
+    {
+        for (int z = -Half_Size; z <= Half_Size; z++){
+        
+			pos_x = x * Ground_Size;
+			pos_z = z * Ground_Size;
+
+			MV1SetPosition(Ground_m_handle, VGet(pos_x, pos_y, pos_z));	  // 位置座標
+			MV1DrawModel(Ground_m_handle);
+        }
+    }
+
+	// ブロックモデルの表示
 	MV1DrawModel(Block_m_handle);
-	//MV1DrawModel(Ground_m_handle);
+	
 }
--- Source/Object.h
+++ Source/Object.h
@@ -13,5 +13,8 @@ private:
 	int	Ground_m_handle; // 地面のハンドル
 	int	Block_m_handle;	 // ブロックのハンドル
 
+	int   Half_Size;     // 地面の表示する範囲
+	float Ground_Size;   // 地面の１マスのサイズ 
+
 	float pos_x, pos_y,	pos_z;	
 };
